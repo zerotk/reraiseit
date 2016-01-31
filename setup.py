83d19941e3f1438b76b8e36843cf060db64bbe64
Alexandre Andrade <kaniabi@gmail.com>
2016-01-31 19:20:41 -0200

    Fix license entry on setup.py.

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
         'Topic :: Software Development :: Libraries :: Python Modules',
 
         # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: LGPL License',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
